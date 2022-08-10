4ceeb822d349ae0a8aefae2f91ad4566c59bed32
JooYoung2274 <jooooo2274@gmail.com>
2022-08-10 17:31:56 +0900

    [add] configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { NestApplicationContextOptions } from '@nestjs/common/interfaces/nest-application-context-options.interface';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { response } from 'express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// AWS 저장소에서 비밀키 불러올때 아래처럼
+// const getEnv = async () => {
+//   // aws 보안저장소에서 비밀키 불러와서 load해주면 비동기로 사용 가능.
+//   const response = axios.get('dfsdfsdf');
+//   return response.data;
+// };
+
+// @Module({
+//   imports: [ConfigModule.forRoot({ isGlobal: true, load: [getEnv] })],
+//   controllers: [AppController],
+//   providers: [AppService, ConfigService],
+// })
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    // console.log(process.env.SECRET);
+    // return process.env.SECRET;
+    // process.env 사용하는것보다 configService을 주입받아 사용하는 것이 환경변수도 nest에게 맡기는걸 더 추천 (IoC)
+    // 테스트할때도 더 좋음
+    console.log(this.configService.get('SECRET'));
+    return this.configService.get('SECRET');
   }
 }
