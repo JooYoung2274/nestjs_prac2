ac517e463cbdb70e2e71c4894eff390c44ea2934
joo2274 <jooyoung@quantumuniverse.co>
2022-08-11 14:40:56 +0900

    [test] github test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { LoggerMiddleware } from 'middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// test
 // AWS 저장소에서 비밀키 불러올때 아래처럼
 // const getEnv = async () => {
 //   // aws 보안저장소에서 비밀키 불러와서 load해주면 비동기로 사용 가능.
