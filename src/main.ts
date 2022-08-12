6a22570b7d58898c1f4989fdb20a35eff374ca45
JooYoung2274 <jooooo2274@gmail.com>
2022-08-12 12:26:26 +0900

    [update] API Swagger example

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 // Hot reload 추가
@@ -6,6 +7,18 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Swagger 추가
+  const config = new DocumentBuilder()
+    .setTitle('slack_clone API')
+    .setDescription('slack_clone coding API Document')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`listening on port ${port}`);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,32 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('api/users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
+  @ApiOperation({ summary: '내 정보조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() body: JoinRequestDto) {
     this.userService.postUsers(body.email, body.nickname, body.password);
   }
 
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
     req.logOut();
