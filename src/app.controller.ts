76a57d8b34f583c45a82150a3f71a052bad4102e
JooYoung2274 <jooooo2274@gmail.com>
2022-08-12 09:44:41 +0900

    [add] dms domain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService, // @Inject('CUSTOM_PROVIDER') private customValue,
-  ) // 만약 custom_provider를 DI하고 싶으면 이렇게 @Inject로. AppService의 경우 그냥 []안에 써줘도 알아서 주입됨
-  {}
+    private readonly appService: AppService, // @Inject('CUSTOM_PROVIDER') private customValue, // 만약 custom_provider를 DI하고 싶으면 이렇게 @Inject로. AppService의 경우 그냥 []안에 써줘도 알아서 주입됨
+  ) {}
 
   @Get()
   async getHello(): Promise<string> {
