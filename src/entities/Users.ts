76944ff25e2519b57a1041cbe0e3fafb824b05ee
JooYoung2274 <jooooo2274@gmail.com>
2022-08-16 09:40:39 +0900

    [update] ManyToMany example

--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -39,6 +39,10 @@ export class Users {
   @UpdateDateColumn()
   updatedAt: Date;
 
+  // Soft Delete. (사용자가 삭제해도 실제로 지우지않음.)
+  // 실제로 아예 삭제해도 되는 데이터가 아닌 복구할수도 있는 데이터라면
+  // deletedAt 의 null 값 유무에 따라서 정함.
+  // 이러면 데이터 불러올 때 deleteAt이 null이 아닌 조건이 필요함.
   @DeleteDateColumn()
   deletedAt: Date | null;
 
@@ -71,6 +75,9 @@ export class Users {
 
   @ManyToMany(() => Workspaces, (workspaces) => workspaces.Members)
   @JoinTable({
+    // ManyToMany
+    // @joinTable 지정할 때 현재 Users class 니까 joinColumn에 UserId 넣고,
+    // 밑에 Workspaces있느니까 inversJoinColumn에 WorkspaceId 넣으면 됨. 그냥 외우는게 좋음
     name: 'workspacemembers',
     joinColumn: {
       name: 'UserId',
@@ -83,6 +90,9 @@ export class Users {
   })
   Workspaces: Workspaces[];
 
+  // ManyToMany
+  // @joinTable 지정할 때 현재 Users class 니까 joinColumn에 UserId 넣고,
+  // 밑에 ChannelId있느니까 inversJoinColumn에 ChannelId 넣으면 됨. 그냥 외우는게 좋음
   @ManyToMany(() => Channels, (channels) => channels.Members)
   @JoinTable({
     name: 'channelmembers',
