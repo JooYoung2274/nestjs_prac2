596967121099d19710c5df9f0085f3c980c059e4
JooYoung2274 <jooooo2274@gmail.com>
2022-08-16 14:34:13 +0900

    [update] entity -> DTO example

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,16 +1,25 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Users } from 'src/entities/Users';
 
-export class JoinRequestDto {
-  @ApiProperty({
-    example: 'joo@gamil.com',
-    description: '이메일',
-    required: true,
-  })
-  public email: string;
+// PickType은 entities에 있는 것을 DTO로 사용하고 싶을때 씀
+export class JoinRequestDto extends PickType(Users, [
+  'email',
+  'nickname',
+  'password',
+] as const) {}
 
-  @ApiProperty({
-    description: '구글 토큰',
-    required: true,
-  })
-  public accessToken: string;
-}
+// // OAUTH 사용할 떄
+// export class JoinRequestDto {
+//   @ApiProperty({
+//     example: 'joo@gamil.com',
+//     description: '이메일',
+//     required: true,
+//   })
+//   public email: string;
+
+//   @ApiProperty({
+//     description: '구글 토큰',
+//     required: true,
+//   })
+//   public accessToken: string;
+// }
